cb76c9dd24c5e208b5bfe49f31862f53fd7f07ff
VeraStasevich <79419166+VeraStasevich@users.noreply.github.com>
2021-03-16 10:55:38 +0300

    Update new_vehicle.js

--- new_vehicle.js
+++ new_vehicle.js
@@ -2,21 +2,19 @@ describe('My First Test', () => {
     it('New vehicle', () => {
         cy.visit('www.vamexpress.by');
         cy.get('#ctl00_MainContent_hlLogIn').click();
-        
+        //login & password
         cy.get('#ctl00_MainContent_tbUserLogin').click();
         cy.get('#ctl00_MainContent_tbUserLogin').type('*****');
-        
         cy.get('#ctl00_MainContent_tbUserPass').click();
         cy.get('#ctl00_MainContent_tbUserPass').type('********');
-        
         cy.get('#ctl00_MainContent_cbRememberMe').check();
-
+        //input
         cy.get('#ctl00_MainContent_bSubmit').click();
-
+        //main menu
         cy.get('#ctl00_MainMenu_hlSouls').click();
         cy.get('#ctl00_MainMenu_hlCars').click();
         cy.get('#ctl00_MainContent_HyperLink1').click();
-
+        //"new vehicale" page
         cy.get('#ctl00_MainContent_tbModel').type('FORD TRANSIT');
         cy.get('#ctl00_MainContent_tbNumber').type('AA 1111-1');
         cy.get('#ctl00_MainContent_tbFirstName').type('Иванов');
@@ -35,11 +33,9 @@ describe('My First Test', () => {
         cy.get('.ui-datepicker-year').select('2022');
         cy.get('#ui-datepicker-div').type('2');
         cy.get('#ctl00_MainContent_tbRegistrationAddress').type('г.Брест, ул. Длинная, д. 1, кв.1');
-        cy.get('#ctl00_MainContent_tbBirthDay').click();
-        cy.get('.ui-datepicker-month').select('9');
-        cy.get('.ui-datepicker-year').select('2011');
-        cy.get('#ui-datepicker-div').type('3');
+        //create
         cy.get('#ctl00_MainContent_btnCreate').click();
+        //check new vehicale and delete
         cy.get('#ctl00_MainContent_stbNumber').type('AA 1111-1');
         cy.get('#ctl00_MainContent_btnSearch').click();
         cy.get('#ctl00_MainContent_lvAllCars_ctrl0_lbDelete').click();
